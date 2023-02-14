caab770300bce06ec1ad0f1a37baac42cf00d3d1
tearex05 <tearex05@yahoo.com>
2023-02-14 23:19:30 +0330

    last

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,8 @@
-import {useEffect, useState} from 'react'
 import {CgShoppingCart} from 'react-icons/cg'
 import {AiFillPhone, AiFillQuestionCircle, AiFillHome}  from 'react-icons/ai'
 
 
 function Nav() {
-	const [color, setColor] = useState('')
 	return (
 		<div className="fixed z-20 flex text-custom2 text-4xl w-screen p-4 items-center justify-between">
 			<a href="#home" className="w-1/6 hover:text-pink-400 transition-x cursor-pointer">
