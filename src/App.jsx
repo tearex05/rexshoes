07f6f7a50b62ce5fa2250a96e709c719d3b16a5a
tearex05 <tearex05@yahoo.com>
2023-02-15 12:13:07 +0330

    last

--- src/App.jsx
+++ src/App.jsx
@@ -15,48 +15,48 @@ function App() {
     <>
     <Nav />
     <div id="home" className="w-screen h-screen flex items-center justify-center text-center relative lg:flex-row-reverse">
-      <div style={{backgroundImage: `url(${Bg})`}} className="img lg:w-1/2 w-screen h-screen absolute lg:relative z-1"></div>
+      <div loading="lazy" style={{backgroundImage: `url(${Bg})`}} className="img lg:w-1/2 w-screen h-screen absolute lg:relative z-1"></div>
       <h1 className="absolute z-2 font-bold sm:text-9xl text-7xl text-white shadow-x lg:w-1/2 lg:relative px-3 h-screen flex flex-col items-center justify-center text-center lg:bg-custom1">Protect Your 2nd <span className="text-red-500 italic pr-3 inline-block">Heart</span></h1>
     </div>
     <div className="w-screen min-h-screen flex items-center justify-center pt-40 bg-custom1">
-     <div style={{backgroundImage: `url(${Pic4})`}} className="w-10/12 h-screen img rounded-3xl flex items-center justify-center text-center">
+     <div loading="lazy" style={{backgroundImage: `url(${Pic4})`}} className="w-full h-screen img flex items-center justify-center text-center">
      <h1 className="sm:text-8xl text-5xl font-bold shadow-x2 text-white px-10 italic">Lots Of Shoes With Different <span className="text-black">Models</span> & <span className="text-black">Colors</span></h1>
      </div>
     </div>
     <div className="w-screen min-h-screen flex flex-col items-center justify-center pt-40 bg-custom1 text-center">
       <h1 className="text-black font-bold text-4xl my-10 shadow-x3">Best Shoes Of The Year</h1>
       <div className="w-screen min-h-screen bg-custom1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center gap-10 px-10">
-      <div style={{backgroundImage: `url(${Pic5})`}} className="img w-full h-full rounded-2xl relative main">
+      <div loading="lazy" style={{backgroundImage: `url(${Pic5})`}} className="img w-full h-full rounded-2xl relative main">
       <div className="text-white shadow-x font-bold text-2xl w-full h-full flex flex-col items-center justify-around absolute bottom-0 p-5 bg-opacity-40 bg-black opacity-0 rounded-2xl on-hover transition-x">
         <p>White Nike</p>
         <p>$100</p>
       </div>
       </div>
-      <div style={{backgroundImage: `url(${Pic2})`}} className="img w-full h-full rounded-2xl relative main">
+      <div loading="lazy" style={{backgroundImage: `url(${Pic2})`}} className="img w-full h-full rounded-2xl relative main">
       <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black  rounded-2xl opacity-0 on-hover transition-x w-full h-full flex flex-col">
         <p>Dark Nike</p>
         <p>$120</p>
       </div>
       </div>
-      <div style={{backgroundImage: `url(${Pic3})`}} className="img w-full h-full rounded-2xl main relative">
+      <div loading="lazy" style={{backgroundImage: `url(${Pic3})`}} className="img w-full h-full rounded-2xl main relative">
       <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black opacity-0 rounded-2xl on-hover transition-x w-full h-full flex flex-col">
         <p>Red Nike</p>
         <p>$90</p>
       </div>
       </div>
-      <div style={{backgroundImage: `url(${Pic6})`}} className="img w-full h-full rounded-2xl relative main">
+      <div loading="lazy" style={{backgroundImage: `url(${Pic6})`}} className="img w-full h-full rounded-2xl relative main">
       <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black opacity-0 rounded-2xl on-hover transition-x w-full h-full flex flex-col">
         <p>Black Nike</p>
         <p>$100</p>
       </div>
       </div>
-      <div style={{backgroundImage: `url(${Pic7})`}} className="img w-full h-full rounded-2xl relative main">
+      <div loading="lazy" style={{backgroundImage: `url(${Pic7})`}} className="img w-full h-full rounded-2xl relative main">
       <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black opacity-0 rounded-2xl on-hover transition-x w-full h-full flex flex-col">
         <p>Gray Nike</p>
         <p>$100</p>
       </div>
       </div>
-      <div style={{backgroundImage: `url(${Pic8})`}} className="img w-full h-full rounded-2xl relative main">
+      <div loading="lazy" style={{backgroundImage: `url(${Pic8})`}} className="img w-full h-full rounded-2xl relative main">
       <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black opacity-0 rounded-2xl on-hover transition-x w-full h-full flex flex-col">
         <p>Colorful Nike</p>
         <p>$100</p>
@@ -65,9 +65,9 @@ function App() {
       </div>
     </div>
     <div className="w-screen bg-custom1 text-5xl lg:text-7xl text-center flex items-center justify-center font-bold pb-36 pt-60">
-      <h1 className="italic pl-3 pr-9 py-6 rounded-3xl w-2/3 shadow-x text-white">Use Code <span className="text-emerald-500 font-bold text-7xl lg:text-9xl">Rx16ew</span> To Get A <span className="text-red-600 font-bold text-7xl lg:text-9xl">50%</span> Off</h1>
+      <h1 className="italic pl-3 pr-9 py-6 rounded-3xl w-2/3 text-white border-4 border-black shadow-b">Use Code <span className="text-emerald-500 font-bold text-6xl lg:text-8xl">Rx16ew</span> To Get A <span className="text-red-600 font-bold text-6xl lg:text-8xl">50%</span> Off</h1>
     </div>
-    <div className="w-screen pt-48 bg-custom1 flex flex-col items-center justify-center text-center pb-12 text-3xl sm:text-5xl md:text-6xl lg:text-8xl font-bold">
+    <div className="w-screen pt-48 bg-custom1 flex flex-col items-center justify-center text-center pb-12 text-3xl sm:text-5xl md:text-6xl lg:text-8xl font-bold px-5">
       <h1>Your Feet Will Thank You</h1>
       <p className="font-normal text-2xl bg-custom2 p-3 mt-5 shadow-b cursor-pointer">Buy Right Now</p>
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,20 @@
+import {useState} from 'react'
 import {CgShoppingCart} from 'react-icons/cg'
 import {AiFillPhone, AiFillQuestionCircle, AiFillHome}  from 'react-icons/ai'
+import {HiMenuAlt3} from 'react-icons/hi'
 
 
 function Nav() {
+	const [style, setStyle] = useState('-translate-x-full')
 	return (
-		<div className="fixed z-20 flex text-custom2 text-4xl w-screen p-4 items-center justify-between">
+		<>
+		<HiMenuAlt3 className="sm:hidden fixed z-20 top-0 right-0 text-custom2 m-4 text-xl cursor-pointer" size="2em" onClick={() => style === '-translate-x-full' ? setStyle('-translate-x-0') : setStyle('-translate-x-full')} />
+		<div className={`sm:hidden fixed z-20 w-2/3 h-screen flex flex-col items-center justify-around text-3xl font-bold italic bg-custom2 shadow-x3 ${style} transition-x2`}>
+			<p className="cursor-pointer hover:text-4xl transition-x">Buy Shoes</p>
+			<p className="cursor-pointer hover:text-4xl transition-x">Contact Us</p>
+			<p className="cursor-pointer hover:text-4xl transition-x">Ask Question</p>
+		</div>
+		<div className="custom-nav z-20 flex text-custom2 text-4xl w-screen p-4 items-center justify-between">
 			<a href="#home" className="w-1/6 hover:text-pink-400 transition-x cursor-pointer">
 				<AiFillHome />
 			</a>
@@ -14,6 +24,7 @@ function Nav() {
 				<AiFillQuestionCircle className="hover:text-pink-400 transition-x cursor-pointer" />
 			</section>
 		</div>
+		</>
 	)
 }
 
