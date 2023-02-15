93d678cb48600acf90ceedf207c3f7df67e1fe56
tearex05 <tearex05@yahoo.com>
2023-02-15 10:37:37 +0330

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Bg from './assets/bg.png'
 import Nav from './components/Nav'
-import Pic5 from './assets/pic5.jpg'
 import Pic2 from './assets/pic2.jpg'
 import Pic3 from './assets/pic3.jpg'
 import Pic4 from './assets/pic4.jpg'
+import Pic5 from './assets/pic5.jpg'
+import Pic6 from './assets/pic6.jpg'
+import Pic7 from './assets/pic7.jpg'
+import Pic8 from './assets/pic8.jpg'
 
 
 function App() {
@@ -20,32 +23,53 @@ function App() {
      <h1 className="sm:text-8xl text-5xl font-bold shadow-x2 text-white px-10 italic">Lots Of Shoes With Different <span className="text-black">Models</span> & <span className="text-black">Colors</span></h1>
      </div>
     </div>
-    <div className="w-screen min-h-screen flex flex-col items-center justify-center pt-40 bg-custom1">
+    <div className="w-screen min-h-screen flex flex-col items-center justify-center pt-40 bg-custom1 text-center">
       <h1 className="text-black font-bold text-4xl my-10 shadow-x3">Best Shoes Of The Year</h1>
-      <div className="w-screen min-h-screen bg-custom1 grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-10 px-10">
+      <div className="w-screen min-h-screen bg-custom1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center gap-10 px-10">
       <div style={{backgroundImage: `url(${Pic5})`}} className="img w-full h-full rounded-2xl relative main">
-      <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black opacity-0 rounded-b-2xl on-hover transition-x">
+      <div className="text-white shadow-x font-bold text-2xl w-full h-full flex flex-col items-center justify-center absolute bottom-0 p-5 bg-opacity-40 bg-black opacity-0 rounded-2xl on-hover transition-x">
         <p>White Nike</p>
         <p>$100</p>
       </div>
       </div>
       <div style={{backgroundImage: `url(${Pic2})`}} className="img w-full h-full rounded-2xl relative main">
-      <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black  rounded-b-2xl opacity-0 on-hover transition-x">
-        <p>Gray Nike</p>
+      <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black  rounded-2xl opacity-0 on-hover transition-x w-full h-full flex flex-col">
+        <p>Dark Nike</p>
         <p>$120</p>
       </div>
       </div>
       <div style={{backgroundImage: `url(${Pic3})`}} className="img w-full h-full rounded-2xl main relative">
-      <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black opacity-0 rounded-b-2xl on-hover transition-x">
+      <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black opacity-0 rounded-2xl on-hover transition-x w-full h-full flex flex-col">
         <p>Red Nike</p>
         <p>$90</p>
       </div>
       </div>
+      <div style={{backgroundImage: `url(${Pic6})`}} className="img w-full h-full rounded-2xl relative main">
+      <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black opacity-0 rounded-2xl on-hover transition-x w-full h-full flex flex-col">
+        <p>Black Nike</p>
+        <p>$100</p>
       </div>
+      </div>
+      <div style={{backgroundImage: `url(${Pic7})`}} className="img w-full h-full rounded-2xl relative main">
+      <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black opacity-0 rounded-2xl on-hover transition-x w-full h-full flex flex-col">
+        <p>Gray Nike</p>
+        <p>$100</p>
+      </div>
+      </div>
+      <div style={{backgroundImage: `url(${Pic8})`}} className="img w-full h-full rounded-2xl relative main">
+      <div className="text-white shadow-x font-bold text-2xl w-full flex items-center justify-around absolute bottom-0 py-5 bg-opacity-40 bg-black opacity-0 rounded-2xl on-hover transition-x w-full h-full flex flex-col">
+        <p>Colorful Nike</p>
+        <p>$100</p>
+      </div>
+      </div>
+      </div>
+    </div>
+    <div className="w-screen bg-custom1 text-5xl lg:text-7xl text-center flex items-center justify-center font-bold pb-36 pt-60">
+      <h1 className="italic pl-3 pr-9 py-6 rounded-3xl w-2/3 shadow-x text-white">Use Code <span className="text-emerald-500 font-bold text-7xl lg:text-9xl">Rx16ew</span> To Get A <span className="text-red-600 font-bold text-7xl lg:text-9xl">50%</span> Off</h1>
     </div>
     <div className="w-screen pt-48 bg-custom1 flex flex-col items-center justify-center text-center pb-12 text-3xl sm:text-5xl md:text-6xl lg:text-8xl font-bold">
       <h1>Your Feet Will Thank You</h1>
-      <a className="font-normal text-2xl bg-custom2 p-3 mt-5 shadow-b">Buy Right Now</a>
+      <p className="font-normal text-2xl bg-custom2 p-3 mt-5 shadow-b cursor-pointer">Buy Right Now</p>
     </div>
     <footer className="text-center bg-custom1 font-bold pt-20 pb-2">Created By Tearex</footer>
     </>
